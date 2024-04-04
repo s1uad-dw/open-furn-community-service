d4b50a810fb40c4d7df972d787f06f9bea23b53e
s1uad-dw <s1uad.dw@gmail.com>
2024-04-04 13:25:28 +0300

    ready

---
+++ src/main/java/ru/s1uad_dw/OpenFurnCommunityService/models/Feedback.java
@@ -0,0 +1,38 @@
+package ru.s1uad_dw.OpenFurnCommunityService.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="feedbacks")
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID userId;
+    private String text;
+    @OneToMany(mappedBy = "feedback", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<FeedbackAnswer> answers;
+    private int evaluation;
+    private Date creationDate;
+    private UUID project;
+
+    public Feedback(UUID userId, String text, int evaluation, UUID project){
+        this.userId = userId;
+        this.text = text;
+        this.answers = new ArrayList<>();
+        this.evaluation = evaluation;
+        this.creationDate = new Date();
+        this.project = project;
+    }
+}
---
+++ src/main/java/ru/s1uad_dw/OpenFurnCommunityService/models/FeedbackAnswer.java
@@ -0,0 +1,32 @@
+package ru.s1uad_dw.OpenFurnCommunityService.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="feedback_answers")
+public class FeedbackAnswer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private UUID userId;
+    private String text;
+    @ManyToOne
+    private Feedback feedback;
+    private Date creationDate = new Date();
+
+    public FeedbackAnswer(UUID userId, String text, Feedback feedback){
+        this.userId = userId;
+        this.text = text;
+        this.feedback = feedback;
+        this.creationDate = new Date();
+    }
+}
